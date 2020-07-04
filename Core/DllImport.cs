f6df1ee193f35c90e62f6effffb94f73b8a88fce
Viruzober <den-viruzober@yandex.ru>
2020-07-04 18:14:24 +0300

    SuperGovnoBuild

--- Core/DllImport.cs
+++ Core/DllImport.cs
@@ -8,8 +8,13 @@ namespace MagicKeys
 [DllImport("user32.dll")]
 [return: MarshalAs(UnmanagedType.Bool)]
 public static extern bool EnumChildWindows(IntPtr hwndParent, EnumChildProcDelegate lpEnumFunc, IntPtr lParam);
-[DllImport("ImageSearchDLL.dll")]
+#if x86
+[DllImport("ImageSearch.dll")]
 public static extern IntPtr ImageSearch(int x, int y, int right, int bottom, string imagePath);
+#elif x64
+[DllImport("ImageSearch64.dll")]
+public static extern IntPtr ImageSearch(int x, int y, int right, int bottom, string imagePath);
+#endif
 [DllImport("user32.dll")]
 public static extern int GetSystemMetrics(int smIndex);
 [DllImport("user32.dll", SetLastError = true)]
@@ -35,30 +40,29 @@ public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int 
 public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 [DllImport("user32.dll")]
 public static extern IntPtr WindowFromPoint(WinPoint p);
+#if x86
 [DllImport("nvdaControllerClient32.dll")]
 public static extern int nvdaController_cancelSpeech();
 [DllImport("nvdaControllerClient32.dll", CharSet = CharSet.Unicode)]
 public static extern int nvdaController_speakText(string text);
-[DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
-public static extern int BitBlt(IntPtr hDc, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+#elif x64
+[DllImport("nvdaControllerClient64.dll")]
+public static extern int nvdaController_cancelSpeech();
+[DllImport("nvdaControllerClient64.dll", CharSet = CharSet.Unicode)]
+public static extern int nvdaController_speakText(string text);
+#endif
 [DllImport("user32.dll", CharSet = CharSet.Auto)]
 public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, IntPtr lParam);
 [DllImport("user32.dll", ExactSpelling = true)]
 public static extern IntPtr GetAncestor(IntPtr hwnd, uint flags);
-[DllImport("user32.dll")]
-[return: MarshalAs(UnmanagedType.Bool)]
-public static extern bool IsWindowEnabled(IntPtr hWnd);
-[DllImport("user32.dll")]
-[return: MarshalAs(UnmanagedType.Bool)]
-public static extern bool IsWindowVisible(IntPtr hWnd);
-[DllImport("user32.dll", SetLastError = true)]
-public static extern IntPtr GetWindow(IntPtr hWnd, uint uCmd);
 [DllImport("user32.dll")]
 public static extern bool GetMenuItemRect(IntPtr hWnd, IntPtr hMenu, uint uItem, out MenuRect lprcItem);
 [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
 public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
 [DllImport("User32.dll")]
 public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
-
+[DllImport("user32.dll")]
+[return: MarshalAs(UnmanagedType.Bool)]
+public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
 }
 }
--- Core/Window/EnumWindowsProc.cs
+++ Core/Window/EnumWindowsProc.cs
@@ -5,10 +5,9 @@ namespace MagicKeys
 {
     public partial class MagicKeys
 {
-[DllImport("user32.dll")]
-[return: MarshalAs(UnmanagedType.Bool)]
-public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
 public delegate bool EnumWindowsProc(IntPtr HWND, IntPtr HWNDList);
+
 public static bool EnumWindowsProcDelegate(IntPtr HWND, IntPtr HWNDList)
 {
 GCHandle gch = GCHandle.FromIntPtr(HWNDList);
