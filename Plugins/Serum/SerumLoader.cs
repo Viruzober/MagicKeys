79204c51693db65cccdd7c364200eb8a419bf21e
Viruzober <den-viruzober@yandex.ru>
2020-07-06 12:04:27 +0300

    serum fix

--- Plugins/Serum/SerumLoader.cs
+++ Plugins/Serum/SerumLoader.cs
@@ -10,16 +10,20 @@ public static partial class Serum
 public static void PluginLoad()
 {
 VUILoader(@PluginsList[0]["VUI"]);
-MouseClick("Left", P[1]+80, P[2]+5, 1, 0, 0, 10);
+MouseClick("Left", P[1]+150, P[2]+20, 1, 0, 0, 10);
 Thread.Sleep(200);
 if (ImgSearch(@"Images\Serum\SerumLogo.bmp")[0] == 1)
 {
 VUIObjectSwitcher(true, "false", 6, 6);
-MouseClick("Left", P[1]+80, P[2]+5, 1, 0, 0, 10);
+}
+else if (ImgSearch(@"Images\Serum\SerumReg.bmp")[0] == 1)
+{
+VUIObjectSwitcher(true, "false", 1, 5);
 }
 else
 {
-VUIObjectSwitcher(true, "false", 1, 5);
+Speak("Перед использованием этого плагина выполните соответствующую инструкцию для него из папки settings");
+return;
 }
 KeyReg();
 }
