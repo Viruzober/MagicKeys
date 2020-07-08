85faf41cf7837bdd73c492888ea92e37cc634246
A11CF0 <a11cf0@yandex.ru>
2020-07-08 18:47:16 +0300

    Revert PixelSearch

--- Core/Images/PixelSearch.cs
+++ Core/Images/PixelSearch.cs
@@ -8,16 +8,24 @@ public partial class MagicKeys
 
 public static bool PixelSearch(int X, int Y, string Color)
 {
-Bitmap Screen = new Bitmap(1, 1);
+Bitmap Screen = new Bitmap(8, 8);
 Graphics g = Graphics.FromImage(Screen);
-g.CopyFromScreen(X, Y, 00, 0, Screen.Size);
-Color GetColor =  Screen.GetPixel(0, 0);
+g.CopyFromScreen(X-4, Y-4, 00, 0, Screen.Size);
+for (int GetX = 0; GetX <= 7; GetX++)
+{
+for (int GetY = 0; GetY <= 7; GetY++)
+{
+Color GetColor =  Screen.GetPixel(GetX, GetY);
 string StrColor = String.Format("#{0:X6}", GetColor.ToArgb() & 0x00FFFFFF);
 if (StrColor == Color)
 {
-return true;
+goto ToOut;
+}
+}
 }
 return false;
+ToOut:
+return true;
 }
 
 }
