3f6038038087eab8f04d2c5bc3fc6d4e1b2b5878
Viruzober <den-viruzober@yandex.ru>
2020-07-14 23:50:40 +0300

    added img for omnisphere

--- Core/Window/WinClose.cs
+++ Core/Window/WinClose.cs
@@ -13,12 +13,12 @@ public static bool WinClose(string HWNDTitle, string HWNDClass)
 SetWindowPos(GetForegroundWindow(), 0, 50, 50, 0, 0, MKC.SWP_NOSIZE|MKC.SWP_NOACTIVATE|MKC.SWP_NOZORDER);
 while(true)
 {
+Thread.Sleep(20);
 P = GetPos();
 if (Array.TrueForAll(P[0..4], V => V == 0))
 {
 return true;
 }
-Thread.Sleep(20);
 Application.DoEvents();
 if (KeySwitch == 1)
 {
--- Plugins/Omnisphere/OmnisphereLoader.cs
+++ Plugins/Omnisphere/OmnisphereLoader.cs
@@ -8,6 +8,14 @@ public static partial class Omnisphere
 
 public static void PluginLoad()
 {
+if (ImgSearch(@"Images\Omnisphere\Reg.bmp", true)[0] == 1)
+{
+VUIObjectSwitcher(true, "false", 1, 1);
+}
+else if (ImgSearch(@"Images\Omnisphere\Logo.bmp", true)[0] == 1)
+{
+VUIObjectSwitcher(true, "false", 2, 4);
+}
 KeyReg();
 }
 
