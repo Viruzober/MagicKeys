0d6f9301fb70ec9d3259d30357ce8a3aafbc84ff
Viruzober <den-viruzober@yandex.ru>
2020-07-18 23:44:03 +0300

    Added help for omnisphere, edded change log.

--- Plugins/Omnisphere/OmnisphereLoader.cs
+++ Plugins/Omnisphere/OmnisphereLoader.cs
@@ -23,6 +23,13 @@ if (ImgSearch(@"Images\Omnisphere\Reg.bmp", true)[0] == 1)
 {
 VUILoader(@"Omnisphere\OmnisphereReg.vui");
 }
+else
+{
+MouseClick("Left", P[1]+220, P[2]+170, 1, 0, 0, 10);
+Thread.Sleep(100);
+MouseClick("Left", P[1]+220, P[2]+400, 1, 0, 0, 10);
+Speak("OK");
+}
 KeyReg();
 }
 
