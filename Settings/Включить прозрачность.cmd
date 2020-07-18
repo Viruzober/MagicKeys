2db7c8867e69b24c97366f1339bb29cb8b3def21
Viruzober <den-viruzober@yandex.ru>
2020-07-18 23:25:54 +0300

    Test Version for Omnisphere

--- Settings/Включить прозрачность.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-Reg Add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v EnableTransparency /t REG_DWORD /d 1 /f
-taskkill /f /im explorer.exe
-start explorer.exe
--- Settings/Отключить прозрачность.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-Reg Add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v EnableTransparency /t REG_DWORD /d 0 /f
-taskkill explorer.exe
-start explorer.exe
