35d11c41b4c85ecb927e5f034523afa551c25381
A11CF0 <a11cf0@yandex.ru>
2020-07-22 13:48:51 +0300

    Add null check in func parser. Attempt 2. Oops.

--- Core/KeyManager/FuncParse.cs
+++ Core/KeyManager/FuncParse.cs
@@ -9,7 +9,11 @@ public partial class MagicKeys
 public static (string, string) FuncParse(string FuncParam)
 {
 string[] Temp = FuncParam.Split("|", 2);
-return (Temp[0], Temp?[1]);
+if (Temp.Length > 1)
+{
+return (Temp[0], Temp[1]);
+}
+return (Temp[0], null);
 }
 
 }
