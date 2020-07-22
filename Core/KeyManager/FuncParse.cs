8dd7a5b6b40bfa4c51fc9caa3bdfafaf423640b1
A11CF0 <a11cf0@yandex.ru>
2020-07-22 13:24:29 +0300

    Fix a terrible typo

--- Core/KeyManager/FuncParse.cs
+++ Core/KeyManager/FuncParse.cs
@@ -9,7 +9,7 @@ public partial class MagicKeys
 public static (string, string) FuncParse(string FuncParam)
 {
 string[] Temp = new string[2];
-Temp = FuncParam.Split("|, 2");
+Temp = FuncParam.Split("|", 2);
 return (Temp[0], Temp[1]);
 }
 
