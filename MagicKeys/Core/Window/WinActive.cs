176e14787a28a4195cc90b497e4b2468a7f724b1
A11CF0 <a11cf0@yandex.ru>
2020-08-02 22:52:33 +0300

    Make window titles and class names case sensitive again and fix some detection issues

--- MagicKeys/Core/Window/WinActive.cs
+++ MagicKeys/Core/Window/WinActive.cs
@@ -11,7 +11,7 @@ public static bool WinActive(string HWNDTitle, string HWNDClass)
 IntPtr Handle = GetForegroundWindow();
 GetWindowText(Handle, Title, nChars);
 GetClassName(Handle, Class, nChars);
-if (Title.ToString().Contains(HWNDTitle, StringComparison.OrdinalIgnoreCase) & Class.ToString().Contains(HWNDClass, StringComparison.OrdinalIgnoreCase))
+if (Title.ToString().Contains(HWNDTitle) & Class.ToString().Contains(HWNDClass))
 {
 return true;
 }
