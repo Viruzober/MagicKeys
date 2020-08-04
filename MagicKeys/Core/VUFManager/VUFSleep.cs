5a85d88ef079fed14afe56f335dea6240c34bce4
A11CF0 <a11cf0@yandex.ru>
2020-08-04 22:57:52 +0300

    Add Sleep to VUF API

---
+++ MagicKeys/Core/VUFManager/VUFSleep.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace MagicKeys
+{
+public partial class MagicKeys
+{
+
+public static void VUFSleep(String Param)
+{
+Thread.Sleep(Convert.ToInt32(Param));
+}
+
+}
+}
