7e0de79c28823010bf56c9baf5637496744af0a5
Viruzober <den-viruzober@yandex.ru>
2020-08-06 13:40:44 +0300

    Added slider function for vuf

---
+++ MagicKeys/Core/VUFManager/VUFSlider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MagicKeys
+{
+public partial class MagicKeys
+{
+
+public static void VUFSlider(string Param)
+{
+string[] FP = Param.Split("|");
+Slider(
+FP[0],
+P[1]+Convert.ToInt32(FP[1]),
+P[2]+Convert.ToInt32(FP[2]),
+FP[3],
+Convert.ToDouble(FP[4]),
+Convert.ToDouble(FP[5])
+);
+}
+
+}
+}
