1d4f9b4497bb700d0a8f6bbc8b2b0f56a52548b1
A11CF0 <a11cf0@yandex.ru>
2020-08-21 21:08:28 +0300

    Add gettext integration for all projects and add template generation

--- Scripts/CompileLocales.cmd
+++ Scripts/CompileLocales.cmd
@@ -1,25 +1,48 @@
 @echo off
 
 setlocal enableextensions enabledelayedexpansion
+set projname=MagicKeys
 
-if "%1" == "" exit /b 1
-if "%2" == "" exit /b 1
+if not "%~1" == "" (
+set rootdir=%~1
+set localedir=!rootdir!\Locale
+) else (
+set rootdir=..
+set localedir=!rootdir!\Locale
+)
+if not "%~2" == "" set destdir=%~2\Locale
 
-set gtdir=%userprofile%\.nuget\packages\gettext.tools
-for /r "%gtdir%" %%i in (msgfmt.exe) do if exist %%i set msgfmt="%%i"
-if "%msgfmt%" == "" (
-echo msgfmt not found
+ if not exist %localedir% (
+echo No locales to process.
 exit /b 1
 )
 
-for /r "%1" %%i in (*.po) do (
-set file=%%i
-"!msgfmt!" -c "!file!" -o "!file:.po=.mo!" || exit /b 1
+set gtdir=%userprofile%\.nuget\packages\gettext.tools
+if defined programfiles(x86) set pogtdir=%programfiles(x86)%\Poedit\GettextTools
+if not defined pogtdir set pogtdir=%programfiles%\Poedit\GettextTools
+
+for /r "%gtdir%" %%i in (.) do if exist "%%i\msgfmt.exe" set gtbindir="%%i"
+if not defined gtbindir if exist "%pogtdir%\bin\msgfmt.exe" set gtbindir=!pogtdir!\bin
+if not defined gtbindir (
+echo Gettext tools not found.
+exit /b 1
 )
 
+set filelist=Files.txt
+for /r "%rootdir%" %%i in (*.cs) do echo %%i >> %filelist%
+"%gtbindir%\xgettext" --force-po --add-comments --from-code=UTF-8 --language=C# --keyword=T._ --package-name=%projname% -o "%localedir%\%projname%.pot" -f %filelist% && del %filelist%
+if errorlevel 1 exit /b 1
+
+for /r "%localedir%" %%i in (*.po) do (
+set file=%%i
+"!gtbindir!\msgmerge" --update "!file!" "!localedir!\!projname!.pot" || exit /b 1
+"!gtbindir!\msgfmt" -c "!file!" -o "!file:.po=.mo!" || exit /b 1
+)
+
+if not defined destdir exit /b
 set excl=Exclude.txt
 echo .po > %excl%
-xcopy /e /i /q /y /exclude:%excl% %1 %2
+xcopy /e /i /q /y /exclude:%excl% "%localedir%" "%destdir%"
 del %excl%
 
 endlocal
