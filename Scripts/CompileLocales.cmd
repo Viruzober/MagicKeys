81fbb9b3080a87cce1a70c0f9505ebc1daade86e
A11CF0 <a11cf0@yandex.ru>
2020-08-22 13:26:06 +0300

    Update NGettext integration script

--- Scripts/CompileLocales.cmd
+++ Scripts/CompileLocales.cmd
@@ -1,13 +1,25 @@
 @echo off
 
 setlocal enableextensions enabledelayedexpansion
-set projname=MagicKeys
+set gtkeyword=T._
+
+if "%1" == "--help" (
+echo The universal NGettext integration script by A11CF0
+echo Usage: %0 [ROOTDIR] [TARGETDIR]
+echo %0 --help
+echo --help displays this help end exits.
+echo ROOTDIR specifies the root directory of the solution in which the 'Locale' folder must be present.
+echo TARGETDIR Specifies the directory to which compiled *.mo files should be copied.
+echo If ROOTDIR is omitted '..' relative to the path of the script is used.
+echo If TARGETDIR is omitted the *.mo files will not be copied.
+exit /b
+)
 
 if not "%~1" == "" (
 set rootdir=%~1
 set localedir=!rootdir!\Locale
 ) else (
-set rootdir=..
+set rootdir=%~dp0\..
 set localedir=!rootdir!\Locale
 )
 if not "%~2" == "" set destdir=%~2\Locale
@@ -29,20 +41,25 @@ exit /b 1
 )
 
 set filelist=Files.txt
+for /r "%localedir%" %%i in (*.po) do if not defined projname set projname=%%~ni
 for /r "%rootdir%" %%i in (*.cs) do echo %%i >> %filelist%
-"%gtbindir%\xgettext" --force-po --add-comments --from-code=UTF-8 --language=C# --keyword=T._ --package-name=%projname% -o "%localedir%\%projname%.pot" -f %filelist% && del %filelist%
-if errorlevel 1 exit /b 1
+"%gtbindir%\xgettext" --force-po --add-comments --from-code=UTF-8 --language=C# --keyword=%gtkeyword% --package-name=%projname% -o "%localedir%\%projname%.pot" -f %filelist%
+set err=%errorlevel%
+if exist %filelist% del %filelist%
+if %err% neq 0 exit /b %err%
 
 for /r "%localedir%" %%i in (*.po) do (
 set file=%%i
-"!gtbindir!\msgmerge" --update "!file!" "!localedir!\!projname!.pot" || exit /b 1
-"!gtbindir!\msgfmt" -c "!file!" -o "!file:.po=.mo!" || exit /b 1
+"!gtbindir!\msgmerge" --update "!file!" "!localedir!\!projname!.pot" || exit /b
+"!gtbindir!\msgfmt" -c "!file!" -o "!file:.po=.mo!" || exit /b
 )
 
 if not defined destdir exit /b
 set excl=Exclude.txt
 echo .po > %excl%
 xcopy /e /i /q /y /exclude:%excl% "%localedir%" "%destdir%"
+set err=%errorlevel%
 del %excl%
+exit /b %err%
 
 endlocal
