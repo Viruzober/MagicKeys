2b787cf83f4be80e0c0e94b3d1ad16262827059b
A11CF0 <a11cf0@yandex.ru>
2020-09-14 00:06:18 +0300

    Better formatting in DeveloperTool output

--- DeveloperTool/Core/Save.cs
+++ DeveloperTool/Core/Save.cs
@@ -10,16 +10,17 @@ public static void Save()
 {
 KeyUnReg();
 OptionKeyUnReg();
-string ITB = MagicKeys.InputTextBox(T._("Save"), T._("Enter name for save."));
-if(ITB == null) return;
+string Name = MagicKeys.InputTextBox(T._("Save"), T._("Enter name for save."));
+if(Name == null) return;
 int[] MP = MagicKeys.GetMousePosition();
 Bitmap Screen = new Bitmap(1, 1);
 Graphics g = Graphics.FromImage(Screen);
 g.CopyFromScreen(MP[0], MP[1], 00, 0, Screen.Size);
 Color GetColor =  Screen.GetPixel(0, 0);
 string StrColor = String.Format("#{0:X6}", GetColor.ToArgb() & 0x00FFFFFF);
-string WR = "X "+(MP[0]-P[1]).ToString()+" Y "+(MP[1]-P[2]).ToString()+" "+StrColor+"\r\n";
-if (OCRR != string.Empty) WR += OCRR+"\r\n";
+string WR = "Name: "+Name+"\r\nX: "+(MP[0]-P[1]).ToString()+"\r\nY: "+(MP[1]-P[2]).ToString()+"\r\nColor: "+StrColor+"\r\n";
+if (OCRR != string.Empty) WR += "OCR:\r\n"+OCRR+"\r\n";
+WR += "\r\n";
 using (StreamWriter sw = new StreamWriter(@".\DevSave\"+Module+@"\"+Module+".txt", true, System.Text.Encoding.Default))
 {
 sw.Write(WR);
