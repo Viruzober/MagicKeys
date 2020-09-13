46faeb88f531ad59b6df1fa12d359df9cc0625a6
A11CF0 <a11cf0@yandex.ru>
2020-09-14 02:09:51 +0300

    Fix empty string handling in ini parser

--- MagicKeys/Core/IniManager/IniRead.cs
+++ MagicKeys/Core/IniManager/IniRead.cs
@@ -23,7 +23,7 @@ return null;
 string[] FileStr = File.ReadAllLines(GetFile);
 for (int S = 0; S <= FileStr.Length-1; S++)
 {
-if (FileStr[S].StartsWith(":") == true || FileStr[S].StartsWith("#") == true) continue;
+if (FileStr[S].StartsWith(":") == true || FileStr[S].StartsWith("#") == true || string.IsNullOrWhiteSpace(FileStr[S])) continue;
 if (FileStr[S].Trim() == "["+GetSection+"]")
 {
 L = S;
@@ -39,7 +39,7 @@ throw new Exception();
 
 for (int R = L+1; R < FileStr.Length; R++)
 {
-if (FileStr[R].StartsWith(":") == true || FileStr[R].StartsWith("#") == true) continue;
+if (FileStr[R].StartsWith(":") == true || FileStr[R].StartsWith("#") == true || string.IsNullOrWhiteSpace(FileStr[R])) continue;
 if (FileStr[R].Contains("[") || R == FileStr.Length-1)
 {
 End = R;
@@ -49,7 +49,7 @@ break;
 
 for (int Start = L+1; Start <= End; Start++)
 {
-if (FileStr[Start].StartsWith(":") == true || FileStr[Start].StartsWith("#") == true) continue;
+if (FileStr[Start].StartsWith(":") == true || FileStr[Start].StartsWith("#") == true || string.IsNullOrWhiteSpace(FileStr[Start])) continue;
 if (FileStr[Start].Contains("[") == true) continue;
 string[] KeyValue = FileStr[Start].Split("=", 2);
 if (KeyValue[0].Trim() == GetKey)
