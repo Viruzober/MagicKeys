cc2c0d3ea4d0e05c1f8b51f08ea7e364bc0acf5d
Viruzober <den-viruzober@yandex.ru>
2021-07-03 16:06:13 +0300

    Added RegImport in ConfigInstaller

--- ConfigInstaller/ConfigInstallerFormActions.cs
+++ ConfigInstaller/ConfigInstallerFormActions.cs
@@ -10,9 +10,6 @@ public partial class ConfigInstallerForm : Form
 
 public void FormLoad(object sender, EventArgs e)
 {
-Directory.CreateDirectory("./Settings");
-Directory.CreateDirectory("./Settings/123");
-Directory.CreateDirectory("./Settings/234");
 string[] Dir = Directory.GetDirectories("./Settings");
 foreach(string d in Dir)
 {
---
+++ ConfigInstaller/RegImport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConfigInstaller
+{
+public partial class ConfigInstaller
+{
+
+public static string RegImport(string PluginName)
+{
+Process proc = new Process();
+try
+{
+proc.StartInfo.FileName = "reg.exe";
+proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+proc.StartInfo.CreateNoWindow = true;
+proc.StartInfo.UseShellExecute = false;
+string command = "import ./Settings/"+PluginName+"/Install.reg";
+proc.StartInfo.Arguments = command;
+proc.Start();
+proc.WaitForExit();
+return "Success";
+}
+catch(Exception)
+{
+return "Error";
+}
+}
+
+}
+}
