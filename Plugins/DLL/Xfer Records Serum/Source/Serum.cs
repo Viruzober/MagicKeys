a90bcd48a27a12897524875d3c706a4e32421a83
Viruzober <den-viruzober@yandex.ru>
2021-07-11 16:28:26 +0300

    Support serum 1.33

--- Plugins/DLL/Xfer Records Serum/Source/Serum.cs
+++ Plugins/DLL/Xfer Records Serum/Source/Serum.cs
@@ -10,12 +10,12 @@ public partial class Serum
 
 public static void PresetsMenu()
 {
-MouseClick("Left", P[1]+330, P[2]+5, 1, 0, 0, 10);
+MouseClick("Left", P[1]+660, P[2]+10, 1, 0, 0, 10);
 }
 
 public static void PreviousPreset()
 {
-MouseClick("Left", P[1]+345, P[2]+5, 1, 0, 0, 10);
+MouseClick("Left", P[1]+690, P[2]+10, 1, 0, 0, 10);
 if (API.GetWClass() == "#32770")
 {
 Speak(ControlGetTextPoint(P[1]+100, P[2]-20));
@@ -24,7 +24,7 @@ Speak(ControlGetTextPoint(P[1]+100, P[2]-20));
 
 public static void NextPreset()
 {
-MouseClick("Left", P[1]+355, P[2]+5, 1, 0, 0, 10);
+MouseClick("Left", P[1]+710, P[2]+10, 1, 0, 0, 10);
 if (API.GetWClass() == "#32770")
 {
 Speak(ControlGetTextPoint(P[1]+100, P[2]-20));
@@ -33,12 +33,12 @@ Speak(ControlGetTextPoint(P[1]+100, P[2]-20));
 
 public static void Menu()
 {
-MouseClick("Left", P[1]+385, P[2]+5, 1, 0, 0, 10);
+MouseClick("Left", P[1]+770, P[2]+10, 1, 0, 0, 10);
 }
 
 public static void SavePresetAs()
 {
-MouseClick("Left", P[1]+220, P[2]+5, 1, 0, 0, 10);
+MouseClick("Left", P[1]+440, P[2]+10, 1, 0, 0, 10);
 }
 
 public static void Register()
@@ -49,10 +49,10 @@ ITBC.InputBoxLabel.Text = T._("Enter activation code.");
 ITBC.ShowDialog();
 if (ITBC.DialogResult == DialogResult.OK)
 {
-MouseClick("Left", P[1]+170, P[2]+20, 1, 0, 0, 10);
-ControlSetTextPoint(P[1]+170, P[2]+20, ITBC.GetString());
-//Thread.Sleep(50);
-//MouseClick("Left", P[1]+355, P[2]+170, 2, 0, 0, 10);
+MouseClick("Left", P[1]+450, P[2]+40, 1, 0, 0, 10);
+ControlSetTextPoint(P[1]+450, P[2]+40, ITBC.GetString());
+Thread.Sleep(50);
+MouseClick("Left", P[1]+710, P[2]+340, 1, 0, 0, 10);
 Speak("please reload the plugin");
 }
 }
--- Plugins/DLL/Xfer Records Serum/Source/SerumLoader.cs
+++ Plugins/DLL/Xfer Records Serum/Source/SerumLoader.cs
@@ -9,18 +9,14 @@ public partial class Serum
 
 public static void SerumLoader()
 {
-        while(ImgSearchArea("Reg", P[1], P[2], P[1]+P[3], P[2]+P[4], 0)[0] == 0)
-{
-if (WinActive(API.GetWTitle(), API.GetWClass()) == false)
-{
-return;
-}
-}
+        Thread.Sleep(100);
 if (ImgSearchArea("Reg", P[1], P[2], P[1]+P[3], P[2]+P[4], 0)[0] == 1)
 {
 VUILoader("Register");
 return;
 }
+if (ImgSearchArea("Logo", P[1], P[2], P[1]+P[3], P[2]+P[4], 0)[0] == 1) return;
+KeyUnReg();
 Speak("Before using this plugin follow the instructions found in the corresponding Settings folder");
 }
 
