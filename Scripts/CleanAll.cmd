90a0b75d41fbd43965fbae724bf39c8b27967da5
A11CF0 <a11cf0.vk@yandex.ru>
2021-07-19 12:57:23 +0300

    Build scripts: Allow non-interactive operation.

--- Scripts/CleanAll.cmd
+++ Scripts/CleanAll.cmd
@@ -5,4 +5,4 @@ dotnet clean -c Release -p:Platform=x64
 if not errorlevel 1 dotnet clean -c Release -p:Platform=x86
 if exist Dist rd /s /q Dist
 popd
-pause
+if /i not "%~1" == "/ni" pause
--- Scripts/PublishAll.cmd
+++ Scripts/PublishAll.cmd
@@ -4,4 +4,4 @@ pushd "%~dp0\.."
 dotnet publish -p:PublishProfile=x64 -c Release -p:Platform=x64
 if not errorlevel 1 dotnet publish -p:PublishProfile=x86 -c Release -p:Platform=x86
 popd
-pause
+if /i not "%~1" == "/ni" pause
