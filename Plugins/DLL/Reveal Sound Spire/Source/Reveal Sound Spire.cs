b3c028591478fa5670f7fbb6449e1bf035859ba0
Viruzober <den-viruzober@yandex.ru>
2021-07-20 17:49:16 +0300

    Added module for spire

---
+++ Plugins/DLL/Reveal Sound Spire/Source/Reveal Sound Spire.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static MagicKeys.MagicKeys;
+
+namespace MagicKeys
+{
+public class RevealSoundSpire
+{
+
+public static void PresetsManager()
+{
+MouseClick("Left", P[1]+325, P[2]+20, 1, 0, 0, 10);
+}
+
+}
+}
