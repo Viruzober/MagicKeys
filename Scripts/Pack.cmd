a40b1001439a01696af4221b25dc196352c6febc
A11CF0 <a11cf0.vk@yandex.ru>
2021-07-21 23:36:49 +0300

    Better status messages in the packer script.

--- Scripts/Pack.cmd
+++ Scripts/Pack.cmd
@@ -6,6 +6,8 @@ pushd "%~dp0.."
 for %%D in ("%cd%\.") do set projname=%%~nxD
 set distdir=Dist
 
+echo Packing...
 for /f "usebackq" %%D in (`dir /b /a:d %distdir%`) do powershell Compress-Archive -Force "%distdir%\%%D\*" "%distdir%\%projname%_%%D.zip"
+echo Done
 popd
 endlocal
