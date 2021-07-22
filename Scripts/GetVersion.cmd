129f520b56c61b39826e021c17a95841a64bc17a
A11CF0 <a11cf0.vk@yandex.ru>
2021-07-22 20:21:21 +0300

    Add GitInfo for version embeding.

--- Scripts/GetVersion.cmd
+++ Scripts/GetVersion.cmd
@@ -3,12 +3,15 @@
 setlocal enableextensions enabledelayedexpansion
 
 git status > nul 2>&1 || exit /b
-for /f "usebackq" %%B in (`git branch --show-current`) do set branch=%%B
-if "%branch%" == "" set branch=unknown
 for /f "usebackq" %%H in (`git rev-parse --short HEAD`) do set hash=%%H
 (
 for /f "usebackq" %%V in (`git describe --exact-match --tags %hash%`) do set version=%%V
 ) > nul 2>&1
-if "%version%" == "" set version=%branch%_%hash%
+if not "%version%" == "" (
+set version=Release_%version%
+) else (
+set version=Nightly_%hash%
+)
+
 echo %version%
 endlocal
