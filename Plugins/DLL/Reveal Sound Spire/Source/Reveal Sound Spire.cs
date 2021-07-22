386f2c9969028424f3c29fe5eca221ecfd4b8dc2
Viruzober <den-viruzober@yandex.ru>
2021-07-22 23:18:55 +0300

    Fix library manager in spire

--- Plugins/DLL/Reveal Sound Spire/Source/Reveal Sound Spire.cs
+++ Plugins/DLL/Reveal Sound Spire/Source/Reveal Sound Spire.cs
@@ -6,7 +6,7 @@ namespace MagicKeys
 {
 public class RevealSoundSpire
 {
-public static string BP = "";
+public static string BP = "Bank";
 public static void Library()
 {
 if (ImgSearch("Lib", true)[0] == 0)
@@ -17,6 +17,7 @@ else
 {
 MouseClick("Left", P[1]+325, P[2]+20, 1, 0, 0, 10);
 }
+Thread.Sleep(200);
 MouseClick("Left", P[1]+200, P[2]+85, 1, 0, 0, 10);
 VUILoader("Library");
 }
@@ -32,13 +33,13 @@ public static void ChangeBank(string Key)
 int[] BImg = ImgSearchArea("Banks", P[1]+50, P[2]+50, P[1]+250, P[2]+500, 20);
 if (BImg[0] == 0)
 {
-MouseClick("Left", P[1]+210, P[2]+85, 1, 0, 0, 10);
+MouseClick("Left", P[1]+210, P[2]+85, 2, 0, 0, 10);
 }
 else
 {
 if (BP == "Preset")
 {
-MouseClick("Left", BImg[5], BImg[6], 1, 0, 0, 10);
+MouseClick("Left", BImg[1], BImg[2], 2, 0, 0, 10);
 BP = "Bank";
 }
 }
@@ -49,22 +50,36 @@ Speak(ImgToText(150, 20, BImg[1], BImg[2], 4));
 
 public static void ChangePreset(string Key)
 {
-BP = "Preset";
+int[] PImg = ImgSearchArea("Presets", P[1]+340, P[2]+50, P[1]+1000, P[2]+500, 20);
+if (PImg[0] == 0)
+{
+MouseClick("Left", P[1]+420, P[2]+80, 1, 0, 0, 10);
+Thread.Sleep(50);
 if (ImgSearchArea("Presets", P[1]+340, P[2]+50, P[1]+1000, P[2]+500, 20)[0] == 0)
 {
+int[] BImg = ImgSearchArea("Banks", P[1]+50, P[2]+50, P[1]+250, P[2]+500, 20);
+MouseClick("Left", BImg[1], BImg[2], 2, 0, 0, 10);
 Speak("Bank not selected");
 return;
 }
+}
+else
+{
+if (BP == "Bank")
+{
+MouseClick("Left", PImg[1], PImg[2], 1, 0, 0, 10);
+BP = "Preset";
+}
+}
 KeySend("Keys", Key, 50, 100);
-int[] BImg = ImgSearchArea("Presets", P[1]+340, P[2]+50, P[1]+1000, P[2]+500, 20);
-Speak(ImgToText(150, 20, BImg[1], BImg[2], 4));
+PImg = ImgSearchArea("Presets", P[1]+340, P[2]+50, P[1]+1000, P[2]+500, 20);
+Speak(ImgToText(150, 20, PImg[1], PImg[2], 4));
 }
 
 public static void SelectBank()
 {
 int[] BImg = ImgSearchArea("Banks", P[1]+50, P[2]+50, P[1]+250, P[2]+500, 20);
 MouseClick("Left", BImg[5], BImg[6], 1, 0, 0, 10);
-MouseClick("Left", P[1]+420, P[2]+80, 1, 0, 0, 10);
 Speak(ImgToText(150, 20, BImg[1], BImg[2], 2)+" selected");
 }
 
