6858e23690c6ce117424e151341f3c6566c80ffe
A11CF0 <a11cf0.vk@yandex.ru>
2021-07-26 20:19:17 +0300

    Automatic cleanup should be run non-interactively.

--- Scripts/PublishAll.cmd
+++ Scripts/PublishAll.cmd
@@ -4,7 +4,7 @@ setlocal enableextensions enabledelayedexpansion
 
 pushd "%~dp0.."
 set distdir=Dist
-if exist "%distdir%" call "%~dp0CleanAll.cmd"
+if exist "%distdir%" call "%~dp0CleanAll.cmd" /ni
 if not errorlevel 1 call "%~dp0Build.cmd" Release x64
 if not errorlevel 1 call "%~dp0Build.cmd" Release x86
 if not errorlevel 1 call "%~dp0Pack.cmd"
