9c8f399411c536f519768fa177e5d8015a3c5c30
Viruzober <den-viruzober@yandex.ru>
2021-08-23 22:51:57 +0300

    Support omnisphere library manager

--- Plugins/DLL/Spectrasonics Omnisphere/Source/OmnisphereLib.cs
+++ Plugins/DLL/Spectrasonics Omnisphere/Source/OmnisphereLib.cs
@@ -6,6 +6,8 @@ using static MagicKeys.MagicKeys;
 using static MKLib;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Linq;
+
 namespace MagicKeys
 {
 
@@ -18,20 +20,34 @@ MouseClick("Left", Coords.X+120, Coords.Y+20, 1, 0, 0, 10);
 VUILoader("LibraryManager");
 }
 
+public static void SelectLibrary()
+{
+MouseClick("Left", Coords.X+60, Coords.Y+60, 1, 0, 0, 10);
+}
+
 public static void LibraryManager()
 {
 int OldX = Coords.X;
-List<OCRResult> TableLines = new List<OCRResult>();
 while(true)
 {
-int[] ColumnSeparator = ImgSearchArea("LibraryManagerLine", OldX, Coords.Y+100, Coords.X+Coords.W, Coords.Y+Coords.H, 50);
+int[] ColumnSeparator = ImgSearchArea("LibraryManagerLine", OldX, Coords.Y+90, Coords.X+Coords.W, Coords.Y+Coords.H, 40);
 if (ColumnSeparator[0] == 0) break;
-OCRResult OCRLibraryManager =  GetOCRResult(ColumnSeparator[1] - OldX, 520, OldX, Coords.Y+100, 1);
-OldX = ColumnSeparator[1]+5;
-TableLines.Add(OCRLibraryManager);
-}
-OmnisphereLibraryManager Manager = new OmnisphereLibraryManager(TableLines);
+OCRResult OCRLibraryManager =  GetOCRResult(ColumnSeparator[1] - OldX, 520, OldX, Coords.Y+90, 2);
+OmnisphereLibraryManager Manager = new OmnisphereLibraryManager(OCRLibraryManager);
 Manager.ShowDialog();
+if (string.IsNullOrEmpty(Manager.GetSelectedValue())) break;
+foreach(var ClickItem in OCRLibraryManager.Lines)
+{
+if (ClickItem.Text == Manager.GetSelectedValue())
+{
+Rect SelectedRect = ClickItem.Words[0].BoundingRect;
+MouseClick("Left", OldX + (SelectedRect.X / 2), ColumnSeparator[2] + (SelectedRect.Y / 2), 1, 0, 0, 20);
+}
+}
+OldX = ColumnSeparator[1] + (ColumnSeparator[3] / 2);
+Thread.Sleep(500);
+}
+Speak("Done");
 }
 
 }
@@ -39,29 +55,51 @@ Manager.ShowDialog();
 public class OmnisphereLibraryManager : Form
 {
 
-public OrderedDictionary<string, ComboBox> LinesComboBoxes = new OrderedDictionary<string, ComboBox>();
+public ComboBox LinesComboBox = new ComboBox();
 
-public OmnisphereLibraryManager(List<OCRResult> OCRLibraryManager)
+public OmnisphereLibraryManager(OCRResult OCRLibraryManager)
 {
 this.StartPosition = FormStartPosition.CenterScreen;
 this.Text = "Omnisphere library manager";
-this.Size = new Size(400, 300);
 TableLayoutPanel Table = new TableLayoutPanel();
-Table.Size = this.Size;
 Table.Location = new Point(0, 0);
 Table.ColumnCount = 2;
-foreach(var OcrItem in OCRLibraryManager)
-{
-Table.Controls.Add(new Label() {Text = "курац"});
-}
+Table.Controls.Add(new Label() {Text = OCRLibraryManager.Lines[0].Text.Trim()});
+string[] ComboBoxValues = OCRLibraryManager.Lines.Select(t => t.Text).ToArray();
+LinesComboBox.Items.AddRange(ComboBoxValues[1..]);
+Table.Controls.Add(LinesComboBox);
 this.Controls.Add(Table);
 this.Load += Form_Load;
+LinesComboBox.KeyDown += Form_KeyDown;
 }
 
 public void Form_Load(object sender, EventArgs e)
 {
-Application.DoEvents();
 this.Activate();
+}
+
+public void Form_KeyDown(object Sender, KeyEventArgs e)
+{
+if (e.KeyCode == Keys.Enter)
+{
+if (LinesComboBox.SelectedIndex == -1)
+{
+Speak("Please select item");
+return;
+}
+this.Close();
+}
+else if (e.KeyCode == Keys.Escape)
+{
+LinesComboBox.SelectedItem = null;
+this.Close();
+}
+}
+
+public string GetSelectedValue()
+{
+if (LinesComboBox.SelectedItem == null) return null;
+return LinesComboBox.SelectedItem.ToString();
 }
 
 }
