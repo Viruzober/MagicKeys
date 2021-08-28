d3e3a87048c93557799ba37151497b7d2b1e77d3
A11CF0 <a11cf0.vk@yandex.ru>
2021-08-28 20:13:20 +0300

    Cosmetic fixes.

--- DeveloperTool/Core/Common/FixWindow.cs
+++ DeveloperTool/Core/Common/FixWindow.cs
@@ -10,7 +10,7 @@ public static void FixWindow()
 {
 if (Coords.X < 0 || Coords.Y < 0 || Coords.W > GetDisplayResolution().Width || Coords.H > GetDisplayResolution().Height)
 {
-DialogResult result = MessageBox.Show(T._("This window apears to be out ov screen. Do you want to move it to the visible aria?"), T._("Attention"), MessageBoxButtons.OKCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+DialogResult result = MessageBox.Show(T._("This window apears to be out off screen. Do you want to move it to the visible area?"), T._("Attention"), MessageBoxButtons.OKCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
 if (result == DialogResult.OK)
 {
 SetWindowPos(GetForegroundWindow(), 0, 50, 50, 0, 0, MKC.SWP_NOSIZE|MKC.SWP_NOACTIVATE|MKC.SWP_NOZORDER);
