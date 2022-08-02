c08b3df5f203f891d979c0c7cf7f2f1efae21854
Denis A. Shishkin <denis.outsidepro@gmail.com>
2022-08-02 17:20:02 +0300

    Fixed plugin syntax

--- Plugins/Lua/Synapse Audio DUNE/Lua/DUNE 3.lua
+++ Plugins/Lua/Synapse Audio DUNE/Lua/DUNE 3.lua
@@ -1,23 +1,23 @@
 function BanksMenu()
-MouseClick("left", 479, 71, 1, 0, 0, 10)
+	MouseClick("left", 479, 71, 1, 0, 0, 10)
 end
 
 function PatchesMenu()
-end
-
-functionck("left", 645, 75, 1, 0, 0, 10)
+	MouseClick("left", 645, 75, 1, 0, 0, 10)
 end
 
 function PresetsMenu()
-MouseClick("left", 455, 102, 1, 0, 0, 10)
+	MouseClick("left", 455, 102, 1, 0, 0, 10)
+end
+
 function PreviousPreset()
-MouseClick("left", 705, 75, 1, 0, 0, 10)
-Sleep(500)
-ImgToText(250, 50, 420, 80, 2)
+	MouseClick("left", 705, 75, 1, 0, 0, 10)
+	Sleep(500)
+	ImgToText(250, 50, 420, 80, 2)
 end
 
 function NextPreset()
-MouseClick("left", 735, 75, 1, 0, 0, 10)
-Sleep(500)
-ImgToText(250, 50, 420, 80, 2)
+	MouseClick("left", 735, 75, 1, 0, 0, 10)
+	Sleep(500)
+	ImgToText(250, 50, 420, 80, 2)
 end
