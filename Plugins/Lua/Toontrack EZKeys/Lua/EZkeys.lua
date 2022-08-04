36d2612280d51e647295a3bdb320a9fa6f6f0dbc
Denis A. Shishkin <denis.outsidepro@gmail.com>
2022-08-04 18:46:18 +0300

    Implement fast presets switching
    There are arrows available so it can be switched quick

--- Plugins/Lua/Toontrack EZKeys/Lua/EZkeys.lua
+++ Plugins/Lua/Toontrack EZKeys/Lua/EZkeys.lua
@@ -6,6 +6,17 @@ function PresetsMenu()
 	MouseClick("Left", 605, 17, 1, 0, 0, 10)
 end
 
+function PresetsSwitch(direction)
+	local arrowsRow = 630
+	local arrowsCol = {
+		up = 21,
+		down = 29
+	}
+	MouseClick("Left", arrowsRow, arrowsCol[direction], 1, 0, 0, 10)
+	Sleep(200)
+	Speak(ImgToText(150, 20, 455, 11, 4))
+end
+
 -- Loader
 Sleep(100)
 if MagicKeys.ImageSearch("Browse")[0] == 0 then
