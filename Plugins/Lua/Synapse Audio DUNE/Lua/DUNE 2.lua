3c061f6139c7ddf719a3a53f0fe264b81fe1b1e9
Denis A. Shishkin <denis.outsidepro@gmail.com>
2022-08-20 06:29:36 +0300

    Rely on screen changes instead of simple waiting in report new loaded preset while preset switching
    Now it reports super fast!
    
    DUNE 2 needs test of current implementation because I haven't this synth version, but dared to code the same as in DUNE 3

--- Plugins/Lua/Synapse Audio DUNE/Lua/DUNE 2.lua
+++ Plugins/Lua/Synapse Audio DUNE/Lua/DUNE 2.lua
@@ -1,23 +1,25 @@
 function BanksMenu()
-	MouseClick("Left", 411, 69, 1, 0, 0, 10)
+	MouseClick("Left", 411, 69, 1, 0, 0, 1)
 end
 
 function PatchesMenu()
-	MouseClick("Left", 525, 75, 1, 0, 0, 10)
+	MouseClick("Left", 525, 75, 1, 0, 0, 1)
 end
 
 function PresetsMenu()
-	MouseClick("Left", 487, 106, 1, 0, 0, 10)
+	MouseClick("Left", 487, 106, 1, 0, 0, 1)
 end
 
 function PreviousPreset()
-	MouseClick("Left", 574, 70, 1, 0, 0, 10)
-	Sleep(500)
+	CreateBitmap(420, 80, 250, 50)
+	MouseClick("Left", 574, 70, 1, 0, 0, 1)
+	DisplayTraffic(420, 80, 250, 50, 5, 5, 2)
 	Speak(ImgToText(250, 50, 420, 80, 2))
 end
 
 function NextPreset()
+	CreateBitmap(420, 80, 250, 50)
 	MouseClick("Left", 596, 71, 1, 0, 0, 10)
-	Sleep(500)
+	DisplayTraffic(420, 80, 250, 50, 5, 5, 2)
 	Speak(ImgToText(250, 50, 420, 80, 2))
 end
--- Plugins/Lua/Synapse Audio DUNE/Lua/DUNE 3.lua
+++ Plugins/Lua/Synapse Audio DUNE/Lua/DUNE 3.lua
@@ -1,23 +1,25 @@
 function BanksMenu()
-	MouseClick("Left", 479, 71, 1, 0, 0, 10)
+	MouseClick("Left", 479, 71, 1, 0, 0, 1)
 end
 
 function PatchesMenu()
-	MouseClick("Left", 645, 75, 1, 0, 0, 10)
+	MouseClick("Left", 645, 75, 1, 0, 0, 1)
 end
 
 function PresetsMenu()
-	MouseClick("Left", 455, 102, 1, 0, 0, 10)
+	MouseClick("Left", 455, 102, 1, 0, 0, 1)
 end
 
 function PreviousPreset()
-	MouseClick("Left", 705, 75, 1, 0, 0, 10)
-	Sleep(500)
+	CreateBitmap(420, 82, 250, 50)
+	MouseClick("Left", 705, 75, 1, 0, 0, 1)
+	DisplayTraffic(420, 82, 250, 50, 5, 5, 2)
 	Speak(ImgToText(250, 50, 420, 80, 2))
 end
 
 function NextPreset()
-	MouseClick("Left", 735, 75, 1, 0, 0, 10)
-	Sleep(500)
+	CreateBitmap(420, 82, 250, 50)
+	MouseClick("Left", 735, 75, 1, 0, 0, 1)
+	DisplayTraffic(420, 82, 250, 50, 5, 5, 2)
 	Speak(ImgToText(250, 50, 420, 80, 2))
 end
